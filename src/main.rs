4c8b2a6bf29ddee4d7115a9b626df065a2d9bbce
toast <58362380+Traumatism@users.noreply.github.com>
2023-03-04 11:06:57 +0100

    Improved code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use reqwest::get;
-use serde_derive::Deserialize;
-use serde_derive::Serialize;
+use serde_derive::{Deserialize, Serialize};
 use std::fs;
 use tokio::io::Result;
 
@@ -30,56 +29,52 @@ async fn main() -> Result<()> {
         "#
     );
 
-    if fs::read_dir("data").is_ok() {
-        print!("~ Removing data/ directory... ");
-        fs::remove_dir_all("data")?;
-        println!("OK");
+    let data_dir = "data";
+
+    if let Ok(_) = fs::read_dir(data_dir) {
+        println!("~ Removing {} directory...", data_dir);
+        fs::remove_dir_all(data_dir)?;
     }
 
-    if std::fs::read_dir("data").is_err() {
-        println!("~ Updating datasets...");
+    println!("~ Updating datasets...");
 
-        print!("    -- Creating data/ directory... ");
-        fs::create_dir("data")?;
-        println!("OK");
+    println!("    -- Creating {} directory...", data_dir);
+    fs::create_dir(data_dir)?;
 
-        print!("    -- Scraping files paths... ");
-        let mut files_urls: Vec<(String, String)> = Vec::new();
-        let mut url = Some("https://www.data.gouv.fr/api/2/datasets/5de8f397634f4164071119c5/resources/?page=1&type=main&page_size=6&q=".into());
+    let mut files_urls = Vec::new();
+    let mut url = Some("https://www.data.gouv.fr/api/2/datasets/5de8f397634f4164071119c5/resources/?page=1&type=main&page_size=6&q=".into());
 
-        while url.is_some() {
-            let response = get(url.unwrap())
-                .await
-                .unwrap()
-                .json::<Response>()
-                .await
-                .unwrap();
+    println!("    -- Scraping files paths...");
+    while let Some(next_url) = url {
+        let response = get(next_url)
+            .await
+            .unwrap()
+            .json::<Response>()
+            .await
+            .unwrap();
 
-            for data in response.data {
-                let re = regex::Regex::new(r"^(deces\-[0-9]+\.txt)$").unwrap();
-                let title = data.title;
+        files_urls.extend(response.data.into_iter().filter_map(|data| {
+            let re = regex::Regex::new(r"^(deces\-[0-9]+\.txt)$").unwrap();
+            let title = data.title;
 
-                if !re.is_match(&title) {
-                    continue;
-                }
-
-                files_urls.push((data.url, title))
+            if !re.is_match(&title) {
+                None
+            } else {
+                Some((data.url, title))
             }
+        }));
 
-            let next_page = response.next_page;
-            url = next_page
-        }
+        url = response.next_page;
+    }
 
-        println!("OK ({} files)", files_urls.len());
-
-        print!("    -- Fetching files... ");
-
-        for (file_url, title) in files_urls {
-            let response = get(file_url).await.unwrap();
-            fs::write(format!("data/{title}"), response.text().await.unwrap()).unwrap();
-        }
-
-        println!("OK (written to data/)");
+    println!("    -- Fetching files...");
+    for (file_url, title) in files_urls {
+        let response = get(file_url).await.unwrap();
+        fs::write(
+            format!("{}/{}", data_dir, title),
+            response.text().await.unwrap(),
+        )
+        .unwrap();
     }
 
     Ok(())
